3f8b61e32ee8cf28c07022d18f22d9895bfa9b01
Yatharth Goyal <yatharthgoyal1@gmail.com>
2021-06-02 13:59:56 +0530

    Chnages in DFS cycle

--- Graphs/CycleInUndirectedGraphUsingDFS.cpp
+++ Graphs/CycleInUndirectedGraphUsingDFS.cpp
@@ -1,20 +1,72 @@
 #include<bits/stdc++.h>
-using namepsace std;
+using namespace std;
 
 class Solution{
 public:
-    bool isCycleUsingDfs(int node, vector<int> adj[], vector<int> &vis, vector<vector<int>> &storeDFS)
+    bool isCycleUsingDfs(int node, int parent, vector<int> adj[], vector<int> &vis)
     {
-        vector<int> parent(node, -1);
-        storeDFS.push_back({});
         vis[node] = 1;
 
         for(auto it : adj[node])
         {
             if(!vis[it])
             {
-                isCycleUsingDfs(it, adj, vis, storeDFS)
+               if(isCycleUsingDfs(it, parent, adj, vis))
+               {
+                   return true;
+                } 
+               else if(it != parent)
+                {
+                    return true;
+                }
             }
         }
+        return false;
+    }
+public:
+    bool isCycle(int node, vector<int> adj[])
+    {
+        vector<int> vis(node, 0);
+        int parent  = -1;
+        for(int i = 0; i < node; i++)
+        {
+            if(!vis[i])
+            {
+                if(isCycleUsingDfs(node, parent,adj, vis));
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
+
+int main()
+{
+    int tc;
+    cin >> tc;
+
+    while(tc--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<int> adj[n+1];
+        
+        for(int i = 0; i < m; i++)
+        {
+             int x ,y;
+            cin >> x >> y;
+
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+
+        Solution obj;
+        bool ans = obj.isCycle(n, adj);
+        if(ans) cout <<"Yes";
+        else cout << "No";
+    }
+
+    return 0;
+}
